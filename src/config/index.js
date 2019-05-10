7d9b4c77f5667f53562b46b74a4c5d187a1565fe
Dauno Martínez <Dauno@C02P4ADYG3QC.group.on>
2019-05-10 14:55:13 -0400

    remove config folder

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-const joi = require('@hapi/joi');
-
-const { value: config } = joi.object().keys({
-  API_URL: joi.string(),
-}).validate(process.env, { stripUnknown: true })
-
-
-module.exports = config;
